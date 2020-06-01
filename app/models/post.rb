a596df92157a062538b78355d14efd61132985cf
Beth Ruff <beth.ruff2@gmail.com>
2020-06-01 22:27:16 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ActiveRecord::Base
   validates :title, presence: true 
   validates :category, inclusion: { in: %w(Fiction Non-fiction) }
-  validates :content, length {  }
+  validates :content, length: { minimum: 2 }
 end
