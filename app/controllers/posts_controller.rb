9c49f4e228ba7c3a450d615f692374c975392ed2
Beth Ruff <beth.ruff2@gmail.com>
2020-06-01 22:42:55 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,9 +2,11 @@ class PostsController < ApplicationController
   before_action :set_post!, only: [:show, :edit, :update]
 
   def show
+    @post.find_by(params[:id])
   end
 
   def edit
+    @post.find_by(params[:id])
   end
 
   def update
